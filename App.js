0f6581fd2d22171e98fe42cf11957bf4955315f0
Nelson Escalante <nescalante92@gmail.com>
2021-04-11 22:28:28 -0400

    add files and components

---
+++ App.js
@@ -0,0 +1,12 @@
+//the function of entire app, this is where everything gets rendered
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Searchform from "./components/Searchform";
+import Table from "./components/Table"; 
+
+// function App() {
+//     return {
+//         <Router> 
+//     }
+// }
